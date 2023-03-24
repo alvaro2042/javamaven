65509d140dab327e09e0350218d44e161bc86de9
alvaro2042 <128509813+alvaro2042@users.noreply.github.com>
2023-03-24 10:12:53 -0500

    Update javamavenDSL2.groovy

--- DSL/javamavenDSL2.groovy
+++ DSL/javamavenDSL2.groovy
@@ -1,7 +1,7 @@
 job('Java Maven App DSL 2') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/alvaro2042/javamaven.git', 'master') { node ->
+        git('https://github.com/alvaro2042/javamaven.git', 'main') { node ->
             node / gitConfigName('alvaro2042')
             node / gitConfigEmail('acamargo@lucasian.com')
         }
